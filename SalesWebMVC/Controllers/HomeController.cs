ee917793cc68374b58664d077d87189327578b76
jhony rodrigues <jhonyherbert@gmail.com>
2023-02-13 20:33:48 -0300

    first controller and razon pages test

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Bem vindo ao primeiro projeto MVC do Jhony!";
+            ViewData["Nome"] = "Jhony Rodrigues";
 
             return View();
         }
